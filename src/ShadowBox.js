df3a662783f6f6476a08d2d6c28bd4ff16290def
Matej Jellus <juffalow@juffalow.com>
2017-12-17 15:39:05 +0100

    Fix shadow box closing problem

--- src/ShadowBox.js
+++ src/ShadowBox.js
@@ -2,10 +2,26 @@ import React, { Component } from 'react';
 import './ShadowBox.css';
 
 class ShadowBox extends Component {
+  constructor(props) {
+    super(props);
+
+    this.onClose = this.onClose.bind(this);
+    this.onContentClick = this.onContentClick.bind(this);
+  }
+
+  onClose(event) {
+    event.stopPropagation();
+    this.props.onClick();
+  }
+
+  onContentClick(event) {
+    event.stopPropagation();
+  }
+
   render() {
     return (
-    <div className="da-shadow-box" onClick={this.props.onClick}>
-      <div className="content">
+    <div className="da-shadow-box" onClick={this.onClose}>
+      <div className="content" onClick={this.onContentClick}>
           {this.props.children}
       </div>
       </div>
